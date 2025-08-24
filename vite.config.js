c8e7a979b243c1dde39d0ae390138a76a3ca178f
Perman Dev <dev12.perman@gmail.com>
2025-08-24 11:40:44 +0700

    add base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/demo-react/',
   define: {
     global: 'globalThis',
     'process.env': {},  // บาง lib เก่าจะอ้างถึง process.env
